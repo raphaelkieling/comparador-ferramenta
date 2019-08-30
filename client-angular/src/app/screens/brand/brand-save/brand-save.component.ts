2cff784003c497c66a94c9822b53b0427dc8b2cd
Raphael Kieling <rapha_kieling@hotmail.com>
2019-08-29 21:38:30 -0300

    Adicionado brand

--- client-angular/src/app/screens/brand/brand-save/brand-save.component.ts
+++ client-angular/src/app/screens/brand/brand-save/brand-save.component.ts
@@ -4,6 +4,7 @@ import { BrandService } from '../brand.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import { Constants } from 'src/app/constants';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-brand-save',
@@ -11,24 +12,30 @@ import { Constants } from 'src/app/constants';
   styleUrls: ['./brand-save.component.scss']
 })
 export class BrandSaveComponent implements OnInit {
-  data: Brand = new Brand();
+  form: FormGroup;
   loading = false;
+  id?: number = null;
 
   constructor(
     private brandService: BrandService,
     private router: Router,
     private active: ActivatedRoute,
-    private snack: MatSnackBar
-  ) { }
+    private snack: MatSnackBar,
+    private builder: FormBuilder
+  ) {
+    this.form = this.builder.group({
+      description: ['', Validators.compose([Validators.required, Validators.maxLength(50)])]
+    })
+  }
 
   ngOnInit() {
-    const id = this.active.snapshot.queryParamMap.get('id');
+    this.id = +this.active.snapshot.queryParamMap.get('id');
 
-    if (!id) { return; }
+    if (!this.id) { return; }
 
     this.loading = true;
-    this.brandService.findOne(id).subscribe((data: Brand) => {
-      this.data = data;
+    this.brandService.findOne(this.id).subscribe((data: Brand) => {
+      this.form.patchValue(data);
     }, err => {
       this.snack.open('Brand not found', '', { duration: Constants.SNACKBAR_TIME });
     }, () => {
@@ -37,7 +44,8 @@ export class BrandSaveComponent implements OnInit {
   }
 
   async beforeSave(): Promise<boolean> {
-    return true;
+    console.log(this.form.get('description'));
+    return !this.form.invalid;
   }
 
   async save() {
@@ -46,10 +54,10 @@ export class BrandSaveComponent implements OnInit {
     if (false === canSave) { return; }
 
     try {
-      if (this.data.id) {
-        await this.brandService.update(this.data.id, this.data).toPromise();
+      if (this.id) {
+        await this.brandService.update(this.id, this.form.value).toPromise();
       } else {
-        await this.brandService.save(this.data).toPromise();
+        await this.brandService.save(this.form.value).toPromise();
       }
       this.snack.open('Success on save!', 'ok', { duration: 2000 })
       this.back();
